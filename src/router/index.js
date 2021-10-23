b999bda92b4455d5a55f1e52b63794719a41d7b9
DevLETi <devleti0106@gmail.com>
2021-10-23 16:52:14 +0900

    ADD Login.vue : 로그인 페이지 추가, 라우터에 login 경로 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
+// views
 import HelloWorld from '@/views/HelloWorld'
+import LoginForm from '@/views/Login'
+
+// component
 import TestComponent from '@/components/TestComponent'
 
 Vue.use(Router)
@@ -16,6 +21,11 @@ export default new Router({
             path: '/test',
             name: 'TestComponent',
             component: TestComponent,
+        },
+        {
+            path: '/login',
+            name: 'LoginForm',
+            component: LoginForm,
         },
     ]
 })
---
+++ src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <p>Log in to your account</p>
+      <form action="" class = "form-login">
+          <input type="email" id="email" value="Email">
+          <input type="password" id="password" value="password">
+          <button type="submit">Login</button>
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
