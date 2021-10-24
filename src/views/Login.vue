dfe3e63dc3011de64b75a4d9231346d4583ec506
DevLETi <devleti0106@gmail.com>
2021-10-24 15:15:56 +0900

    EDIT Login.vue : 중앙 정렬 및 스타일 적용

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
-      <p>Log in to your account</p>
-      <form action="" class = "form-login">
-          <input type="email" id="email" value="Email">
-          <input type="password" id="password" value="password">
-          <button type="submit">Login</button>
+  <div class = "container">
+    <!-- GNB를 위해 div 한번 더 나누기 -->
+    <!-- <GNB /> -->
+    <div class="login">
+      <p class = "title">로그인</p>
+      <form action="">
+        <input type="email" id="email" placeholder="이메일">
+        <input type="password" id="password" placeholder="패스워드">
+        <button type="submit">Login</button>
       </form>
+    </div>
   </div>
 </template>
 
@@ -15,6 +19,33 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.container {
+  height: 98vh;
+  .login{
+    display: flex;
+    flex-direction: column; // 수직으로 배치
+    justify-content: center; // 상하 정렬
+    max-width: 280px;
+    min-height: 100%;
+    margin: 0 auto; // 좌우 정렬
+    .title {
+      margin-top: 0;
+      text-align: center;
+      font-weight: 700;
+      font-size: 20px;
+    }
+    form {
+      input {
+          width: 93%;
+          padding : 8px;
+          margin-bottom: 10px;
+        }
+      button {
+        width: 100%;
+        padding : 8px;
+      }
+    }
+  }
+}
 </style>
