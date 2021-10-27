80e19a1ecbd5a9d334f71a97d5829553334c1499
DevLETi <devleti0106@gmail.com>
2021-10-27 11:22:08 +0900

    의사 페이지 vue-cal 적용

--- src/views/Doctor_main.vue
+++ src/views/Doctor_main.vue
@@ -12,44 +12,41 @@
           </div>
       </div>
       <div class="calendar">
-          <kalendar :configuration="calendar_settings" :events.sync="events" />
+          <vue-cal 
+            :time-from="7*60"
+            :time-to="22*60"
+            :time-step="60"
+            active-view="week"
+            :disable-views="['years', 'year', 'day']"
+            locale="ko"
+            :events="events"
+          />
       </div>
   </div>
 </template>
 
 <script>
-import Vue from "vue";
-import PortalVue from "portal-vue";
-Vue.use(PortalVue);
-import { Kalendar } from 'kalendar-vue';
+import VueCal from 'vue-cal'
+import 'vue-cal/dist/i18n/ko.js'
+import 'vue-cal/dist/vuecal.css'
 
 export default {
     name: 'Doctor_main',
     components: {
-        Kalendar,
+        VueCal
     },
     data: () => ({
-        calendar_settings: {
-        style: 'material_design',
-        view_type: 'week',
-        cell_height: 8,
-        scrollToNow: true,
-        start_day: new Date().toISOString(),
-        read_only: false,
-        day_starts_at: 8,
-        day_ends_at: 22,
-        overlap: true,
-        hide_dates: [], // Spooky
-        hide_days: [],
-        past_event_creation: true
-        },
         events: [
-            {
-                from: '2021-10-27T18:00:00Z',
-                to: '2021-10-27T19:00:00Z',
-                data: 'Event 1',
-            },
-        ],
+        {
+        start: '2021-10-27 12:30',
+        end: '2021-10-27 13:30',
+        // You can also define event dates with Javascript Date objects:
+        // start: new Date(2018, 11 - 1, 16, 10, 30),
+        // end: new Date(2018, 11 - 1, 16, 11, 30),
+        title: 'Doctor appointment',
+        content: '<i class="v-icon material-icons">local_hospital</i>',
+        class: 'health'
+        }]
     })
 }
 </script>
@@ -57,7 +54,8 @@ export default {
 <style lang="scss" scoped>
 .container {
     display: flex;
-    width : 100%;
+    width: 100%;
+    padding-top: 60px; //gnb 침범하는 문제 해결
     .sidebar {
         width: 20%;
         border-right: 1px solid #000;
@@ -67,10 +65,15 @@ export default {
         }
     }
     .calendar {
-        width : 80%;
-        // kalendar height 수정
-        .kalendar-wrapper {
-            min-height: 100vh;
+        width: 80%;
+        vue-cal {
+            .vuecal__event {
+                .health {
+                    background-color: rgba(255, 102, 102, 0.9);
+                    border: 1px solid rgb(235, 82, 82);
+                    color: #fff;
+                }            
+            }
         }
     }
 }
