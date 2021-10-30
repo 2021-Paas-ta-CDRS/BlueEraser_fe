bfea22d336998f8034376c6afa50e5d69eb89c47
JoonShik <jshong0907@naver.com>
2021-10-30 14:33:54 +0900

    FIX:router link 설정

--- src/views/GNB.vue
+++ src/views/GNB.vue
@@ -2,12 +2,18 @@
     <div class="gnb">
         <vs-navbar center-collapsed v-model="active">
             <template #left>
-                <img src="@/assets/logo_partial_blue.png" alt="" style="height: 30px;" class="logo">
+                <router-link to="/" class="button">
+                    <img src="@/assets/logo_partial_blue.png" alt="" style="height: 30px;" class="logo">
+                </router-link>
             </template>
             
             <template #right>
-                <vs-button flat >로그인</vs-button>
-                <vs-button>회원가입</vs-button>
+                <router-link to="/login" class="button">
+                    <vs-button flat>로그인</vs-button>
+                </router-link>
+                <router-link to="/signup" class="button">
+                    <vs-button>회원가입</vs-button>
+                </router-link>
             </template>
         </vs-navbar>
     </div>
@@ -19,3 +25,8 @@
     })
 }
 </script>
+<style lang="scss" scoped>
+.button {
+    @include button;
+}
+</style>
