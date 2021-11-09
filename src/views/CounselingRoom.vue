e40b4130d5ab535b72bb8498f38f614fea434b90
DevLETi <devleti0106@gmail.com>
2021-11-09 21:16:12 +0900

    vue-webrtc 관련 주석 추가

--- src/views/CounselingRoom.vue
+++ src/views/CounselingRoom.vue
@@ -1,20 +1,21 @@
 <template>
+    <!-- 이후 roomId를 조정해 개인실로 만들기 -->
     <vue-webrtc ref="webrtc" width="100%" roomId="234132"/>
 </template>
 
 <script>
+/* vue-webrtc를 위한 import 및 코드 */
 import Vue from 'vue'
 import WebRTC from 'vue-webrtc'
-// ISSUE 5: https://github.com/westonsoftware/vue-webrtc/issues/5
 import * as io from 'socket.io-client'
 window.io = io
-//
 Vue.use(WebRTC)
+/* vue-webrtc 구동용 코드 끝 */
 
 export default {
     name: 'CounselingRoom',
     mounted: function() {
-        this.$refs.webrtc.join()
+        this.$refs.webrtc.join() // vue-webrtc 구동을 위한 함수입니다.
     }
 }
 </script>
