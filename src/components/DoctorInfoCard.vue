2cbc822d1d1f083bb5226df4f07597fb79d1e9ed
Junseok Yoon <phobyjun@gmail.com>
2021-11-09 20:25:03 +0900

    ADD: add DoctorInfoCard component

---
+++ src/components/DoctorInfoCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div v-for="doctor in doctors" v-bind:key='doctor'>{{ doctor.name }}</div>
+    </div>
+</template>
+
+<script>
+import { getDoctorList } from '../api/index.js'
+
+export default {
+    data() {
+        return {
+            doctors: []
+        }
+    },
+    created() {
+        getDoctorList()
+            .then(response => this.doctors = response.data)
+            .catch()
+    }
+}
+</script>
