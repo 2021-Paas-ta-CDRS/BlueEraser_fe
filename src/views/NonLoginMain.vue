8d8a6a13ba447fa381a06c7bfc127810550ff2b8
Junseok Yoon <phobyjun@gmail.com>
2021-11-10 21:33:29 +0900

    :ADD 메인 이미지 삽입

--- src/views/NonLoginMain.vue
+++ src/views/NonLoginMain.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="main">
+        <img src="@/assets/main_image.jpg" />
         <div class="containter">
+            <h2>당신에게 꼭 맞는 의사를 찾아보세요</h2>
             <DoctorInfoCard />
         </div>
     </div>
@@ -19,9 +21,15 @@ export default {
 <style lang="scss">
 .main {
     display: flex;
-    justify-content: center;
-}
-.containter {
-    width: 70%;
+    flex-direction: column;
+    align-items: center;
+    .containter {
+        width: 85%;
+    }   
+    img {
+        width: 100%;
+        height: 500px;
+        object-fit: cover;
+    }
 }
 </style>
