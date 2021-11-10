56026d81314d10ad60f63acd7ce9ee10ebf829f2
Junseok Yoon <phobyjun@gmail.com>
2021-11-10 16:47:37 +0900

    ADD: 비로그인 메인화면 views 추가

--- src/router/index.js
+++ src/router/index.js
@@ -2,18 +2,16 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 // views
-import HelloWorld from '@/views/HelloWorld'
 import Login from '@/views/Login'
 import Signup from '@/views/Signup'
 import SignupDoctor from '@/views/SignupDoctor'
 import SignupUser from '@/views/SignupUser'
 import DoctorMain from '@/views/DoctorMain'
+import NonLoginMain from '@/views/NonLoginMain'
 
 // component
 import TestComponent from '@/components/TestComponent'
 import MyPageDoctor from '@/components/MyPageDoctor'
-
-import DoctorInfoCard from '@/components/DoctorInfoCard'
 
 Vue.use(Router)
 
@@ -21,8 +19,8 @@ export default new Router({
     routes: [
         {
             path: '/',
-            name: 'DoctorInfoCard',
-            component: DoctorInfoCard
+            name: 'NonLoginMain',
+            component: NonLoginMain
         },
         {
             path: '/test',
---
+++ src/views/NonLoginMain.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="main">
+        <div class="containter">
+            <DoctorInfoCard />
+        </div>
+    </div>
+</template>
+
+<script>
+import DoctorInfoCard from '@/components/DoctorInfoCard'
+
+export default {
+    components: { 
+        DoctorInfoCard 
+    }
+}
+</script>
+
+<style lang="scss">
+.main {
+    display: flex;
+    justify-content: center;
+}
+.containter {
+    width: 70%;
+}
+</style>
