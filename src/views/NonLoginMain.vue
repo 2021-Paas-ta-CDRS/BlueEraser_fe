fdbaab64515b8d6750738bc7574bc7ffa344f6b0
Junseok Yoon <phobyjun@gmail.com>
2021-11-16 21:42:28 +0900

    FIX: mobile view fix

--- src/views/NonLoginMain.vue
+++ src/views/NonLoginMain.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="main">
         <img src="@/assets/main_image.jpg" />
-        <div class="containter">
+        <div class="container">
             <h2>당신에게 꼭 맞는 의사를 찾아보세요</h2>
             <DoctorInfoCard />
         </div>
@@ -23,13 +23,21 @@ export default {
     display: flex;
     flex-direction: column;
     align-items: center;
-    .containter {
+    .container {
         width: 85%;
     }   
     img {
         width: 100%;
         height: 500px;
         object-fit: cover;
+    }
+}
+
+@media screen and (max-width: 768px) {
+    .main {
+        .container {
+            width: 100%;
+        }
     }
 }
 </style>
