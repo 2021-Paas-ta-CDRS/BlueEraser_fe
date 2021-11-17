97bc6f8543e2a5cc16e3324073b63e751d5e72e3
DevLETi <devleti0106@gmail.com>
2021-11-17 14:46:33 +0900

    tiptap v2 -> v1 교체, 텍스트 스타일 메뉴 적용

--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -1,72 +1,268 @@
 <template>
-  <editor-content :editor="editor" />
+  <div class="editor">
+    <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
+      <div class="menubar">
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.bold() }"
+          @click="commands.bold"
+        >
+          <img class="icon" src="@/assets/tiptap/icons/bold.svg" alt="" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.italic() }"
+          @click="commands.italic"
+        >
+          <img class="icon" src="@/assets/tiptap/icons/italic.svg" alt="" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.strike() }"
+          @click="commands.strike"
+        >
+          <img class="icon" src="@/assets/tiptap/icons/strike.svg" alt="" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.underline() }"
+          @click="commands.underline"
+        >
+          <img class="icon" src="@/assets/tiptap/icons/underline.svg" alt="" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.code() }"
+          @click="commands.code"
+        >
+          <img class="icon" src="@/assets/tiptap/icons/code.svg" alt="" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.paragraph() }"
+          @click="commands.paragraph"
+        >
+          <img class="icon" src="@/assets/tiptap/icons/paragraph.svg" alt="" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.heading({ level: 1 }) }"
+          @click="commands.heading({ level: 1 })"
+        >
+          H1
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.heading({ level: 2 }) }"
+          @click="commands.heading({ level: 2 })"
+        >
+          H2
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.heading({ level: 3 }) }"
+          @click="commands.heading({ level: 3 })"
+        >
+          H3
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.bullet_list() }"
+          @click="commands.bullet_list"
+        >
+          <img class="icon" src="@/assets/tiptap/icons/ul.svg" alt="" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.ordered_list() }"
+          @click="commands.ordered_list"
+        >
+          <img class="icon" src="@/assets/tiptap/icons/ol.svg" alt="" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.blockquote() }"
+          @click="commands.blockquote"
+        >
+          <img class="icon" src="@/assets/tiptap/icons/quote.svg" alt="" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.code_block() }"
+          @click="commands.code_block"
+        >
+          <img class="icon" src="@/assets/tiptap/icons/code.svg" alt="" />
+        </button>
+
+        <button
+          class="menubar__button"
+          @click="commands.horizontal_rule"
+        >
+          <img class="icon" src="@/assets/tiptap/icons/hr.svg" alt="" />
+        </button>
+
+        <button
+          class="menubar__button"
+          @click="commands.undo"
+        >
+          <img class="icon" src="@/assets/tiptap/icons/undo.svg" alt="" />
+        </button>
+
+        <button
+          class="menubar__button"
+          @click="commands.redo"
+        >
+          <img class="icon" src="@/assets/tiptap/icons/redo.svg" alt="" />
+        </button>
+
+      </div>
+    </editor-menu-bar>
+
+    <editor-content class="editor__content" :editor="editor" />
+  </div>
 </template>
 
 <script>
-import { Editor, EditorContent } from '@tiptap/vue-2'
-import StarterKit from '@tiptap/starter-kit'
-import Placeholder from '@tiptap/extension-placeholder'
+//import Icon from '@/components/Icon.vue'
+import { Editor, EditorContent, EditorMenuBar } from 'tiptap'
+import '@/styles/tiptap/main.scss'
+import {
+  Blockquote,
+  CodeBlock,
+  HardBreak,
+  Heading,
+  HorizontalRule,
+  OrderedList,
+  BulletList,
+  ListItem,
+  TodoItem,
+  TodoList,
+  Bold,
+  Code,
+  Italic,
+  Link,
+  Strike,
+  Underline,
+  History,
+  Placeholder,
+} from 'tiptap-extensions'
 
 export default {
   components: {
     EditorContent,
+    EditorMenuBar,
+    //Icon,
   },
-
   data() {
     return {
-      editor: null,
+      editor: new Editor({
+        extensions: [
+          new Blockquote(),
+          new BulletList(),
+          new CodeBlock(),
+          new HardBreak(),
+          new Heading({ levels: [1, 2, 3] }),
+          new HorizontalRule(),
+          new ListItem(),
+          new OrderedList(),
+          new TodoItem(),
+          new TodoList(),
+          new Link(),
+          new Bold(),
+          new Code(),
+          new Italic(),
+          new Strike(),
+          new Underline(),
+          new History(),
+          new Placeholder({
+            emptyEditorClass: 'is-editor-empty',
+            emptyNodeClass: 'is-empty',
+            emptyNodeText: '이곳에 진료기록/경과기록을 적어주시면 됩니다.',
+            showOnlyWhenEditable: true,
+            showOnlyCurrent: true,
+          }),
+        ],
+        content: `
+          
+        `,
+      }),
     }
   },
-
-  mounted() {
-    this.editor = new Editor({
-      extensions: [
-        StarterKit,
-        Placeholder.configure({
-          // Use a placeholder:
-          placeholder: '이곳에 진료/경과관찰을 남기시면 됩니다.',
-          // Use different placeholders depending on the node type:
-          // placeholder: ({ node }) => {
-          //   if (node.type.name === 'heading') {
-          //     return 'What’s the title?'
-          //   }
-
-          //   return 'Can you add some further context?'
-          // },
-        }),
-      ],
-    })
-  },
-
-  beforeUnmount() {
+  beforeDestroy() {
     this.editor.destroy()
   },
 }
 </script>
 
-<style lang="scss">
-/* Basic editor styles */
-.ProseMirror {
-  > * + * {
-    margin-top: 0.75em;
+<style lang="scss" scope>
+.icon {
+  position: relative;
+  display: inline-block;
+  vertical-align: middle;
+  width: 0.8rem;
+  height: 0.8rem;
+  margin: 0 .3rem;
+  top: -.05rem;
+  fill: currentColor;
+
+  // &.has-align-fix {
+  // 	top: -.1rem;
+  // }
+
+  &__svg {
+    display: inline-block;
+    vertical-align: top;
+    width: 100%;
+    height: 100%;
+  }
+
+  &:first-child {
+    margin-left: 0;
+  }
+
+  &:last-child {
+    margin-right: 0;
+  }
+
+}
+
+// svg sprite
+body > svg,
+.icon use > svg,
+symbol {
+  path,
+  rect,
+  circle,
+  g {
+    fill: currentColor;
+    stroke: none;
+  }
+
+  *[d="M0 0h24v24H0z"] {
+    display: none;
   }
 }
-
-/* Placeholder (at the top) */
-.ProseMirror p.is-editor-empty:first-child::before {
-  content: attr(data-placeholder);
+ 
+.editor p.is-editor-empty:first-child::before {
+  content: attr(data-empty-text);
   float: left;
-  color: #adb5bd;
+  color: #aaa;
   pointer-events: none;
   height: 0;
+  font-style: italic;
 }
 
-/* Placeholder (on every new line) */
-/*.ProseMirror p.is-empty::before {
-  content: attr(data-placeholder);
-  float: left;
-  color: #adb5bd;
-  pointer-events: none;
-  height: 0;
-}*/
 </style>
