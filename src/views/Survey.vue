7adc3c1721c7fbf3e43d0ba3e67999e04fab163e
DevLETi <devleti0106@gmail.com>
2022-01-03 20:35:04 +0900

    불필요한 console.log 제거, answers index 자동 추가 함수 추가

--- src/views/Survey.vue
+++ src/views/Survey.vue
@@ -4,11 +4,11 @@
       <div v-for="(question, i) in questions" :key="question">
           <p class="question">{{parseInt(i) + 1}} : {{question}}</p>
           <div class="selectRadio">
-              <vs-radio v-model="answers[parseInt(i)]" val="5">매우 그렇다</vs-radio>
-              <vs-radio v-model="answers[parseInt(i)]" val="4">조금 그렇다</vs-radio>
-              <vs-radio v-model="answers[parseInt(i)]" val="3">보통이다</vs-radio>
-              <vs-radio v-model="answers[parseInt(i)]" val="2">조금 아니다</vs-radio>
               <vs-radio v-model="answers[parseInt(i)]" val="1">매우 아니다</vs-radio>
+              <vs-radio v-model="answers[parseInt(i)]" val="2">조금 아니다</vs-radio>
+              <vs-radio v-model="answers[parseInt(i)]" val="3">보통이다</vs-radio>
+              <vs-radio v-model="answers[parseInt(i)]" val="4">조금 그렇다</vs-radio>
+              <vs-radio v-model="answers[parseInt(i)]" val="5">매우 그렇다</vs-radio>
           </div>
           <br/>
       </div>
@@ -30,17 +30,23 @@ export default {
     name: 'Survey',
     data:() => ({
       questions: {},
-      //TODO: answers의 index를 직접 안만들고 바로 접근할 수 있는 방법?
-      answers: ['','','','','','','','','','','','','','','','','','','',''],
+      answers: [],
     }),
     methods: {
       //TODO: form submit 되었을 때 응답값 저장하는 api 호출하기
       // 비어있는 응답이 없도록 확인하는 로직도 같이
+      makeAnswersIdx() {
+          var questionsLen = Object.keys(this.questions).length; // json은 바로 length로 구할 수 없음. key의 개수를 세는 방식으로 우회
+          for(var i = 0; i < questionsLen; i++) {
+              this.answers.push('')
+          }
+      }
     },
     created() {
         getQuestionnaire()
         .then(res => {
             this.questions = res
+            this.makeAnswersIdx();
         })
         .catch(err => { console.log(err)})
     }
